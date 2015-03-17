47a034b266c4157bcd974ba31c04a7bc34e8818a
Edel Altares <edel.altares@gmail.com>
2015-03-16 23:39:53 -0600

    Created NetBeans project, rough home page

--- header.php
+++ header.php
@@ -4,9 +4,20 @@
 
 <title>Our Library</title>
 
+<link rel="stylesheet" href="style.css" type="text/css" />
+
 </head>
 
 <body>
 
-<h1>Our Library</h1>
-<h2>This is our library!</h2>
+<h1>Public Library</h1>
+
+<div class="nav">
+    <ul class="navigation">
+        <li><a href="#">Books</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">Login</a></li>
+    </ul>
+</div>
+
+<div class="content">
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php include('header.php'); ?>
 
+<h2>Welcome</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin eget mauris ac sagittis. Donec convallis vestibulum sollicitudin. Proin sit amet metus vitae ligula convallis tincidunt. Cras pharetra sagittis turpis, a maximus ipsum. Maecenas a pharetra nunc. Vestibulum ultrices odio a dolor efficitur, eget dignissim sapien facilisis. Praesent quam turpis, efficitur et dapibus quis, egestas a massa.</p>
+
+<p>Maecenas scelerisque vulputate ex mattis tincidunt. Vivamus quis venenatis nisl, nec tempor nibh. Vivamus dictum tortor a dictum venenatis. Fusce arcu nisl, ullamcorper id lacus sed, consectetur suscipit justo. Nunc sit amet lorem sit amet mi lobortis vestibulum. Aenean aliquam placerat libero nec placerat. Nullam ac magna luctus lorem hendrerit elementum. Quisque porta scelerisque dolor, eu rhoncus orci hendrerit id. Nulla facilisi. Mauris pharetra pulvinar dolor. Mauris facilisis, tortor sit amet tincidunt suscipit, lacus sapien hendrerit justo, non congue nisi est nec libero. Aenean nisi dolor, consequat vitae eros nec, pulvinar iaculis lectus.</p>
+
 <?php include('footer.php'); ?>
