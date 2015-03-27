8e2e07c0432f8a746591c3778db7912f4b7d3cb9
Edel Altares <edel.altares@gmail.com>
2015-03-26 23:47:29 -0600

    Updates Lib_schema.sql
    - Cleaned up code to look prettier
    - Finished missing table code

--- Lib_Schema.sql
+++ Lib_Schema.sql
@@ -4,44 +4,23 @@ use Library;
 create table Patron
 (
 
-Card_No	int(8) Primary key,
-FName	varchar(255),
-LName	varchar(255),
-Email	varchar(255),
-Address	varchar(255),
-city	varchar(255),
-Pcode	varchar(255),
-CrdExp	date not null,
-Accnt_Type	varchar(255) not null
 
 );
 
 create table Staff
 (
 
-SIN	int(9) Primary Key,
-FName	varchar(255),
-LName	varchar(255),
-Email	varchar(255),
-Address	varchar(255),
-city	varchar(255),
-Pcode	varchar(255),
-Wage	int(255),
 Position	varchar(255),
-SuperSIN int(9),
-foreign key (SuperSIN) references Staff(SIN)
 
 );
 
 create table Branch
 (
 
-BranchNo	int(3) Primary Key,
 BranchName	varchar(255),
 PhoneNo		int(10),
 Address		varchar(255),
 ManagerSIN	int(9),
-foreign key	(ManagerSIN) references Staff(SIN)
 
 );
 
@@ -53,23 +32,14 @@ EventName	varchar(255),
 EDate		datetime,
 Description	text,
 BranchNum	int(3),
-foreign key (BranchNum) references Branch(BranchNo)
 
 );
 
 create table Book
 (
 
-BookNo	int(10) primary key,
-ISBN	int(13),
-CallNo	varchar(255),
-Title	varchar(255),
-Summary	text,
 PatronNo	int(8),
-foreign key (PatronNo) references Patron(CardNo),
-DueDate	date,
 BranchNum	int(3),
-foreign key (BranchNum) references Branch(BranchNo)
 
 );
 
@@ -77,9 +47,6 @@ foreign key (BranchNum) references Branch(BranchNo)
 create table Journal
 (
 
-JBookNo	int(10) primary key,
-foreign key (JBookNo) references Book(BookNo),
-Institution	varchar(255)
 
 );
 
@@ -87,10 +54,7 @@ Institution	varchar(255)
 create table Audio_Book
 (
 
-ABookNo	int(10) primary key,
-foreign key (ABookNo) references Book(BookNo),
 Narrator	varchar(255),
-length	time
 
 );
 
@@ -98,16 +62,12 @@ length	time
 create table Author
 (
 
-Fname	varchar(255),
-MName	varchar(255),
-LName	varchar(255)
 
 );
 
 
 create table Publisher
 (
-PublisherName	varchar(255) primary key
 );
 
 
@@ -115,10 +75,6 @@ create table Publisher_Books
 (
 
 PublisherName	varchar(255),
-foreign key (PublisherName) references Publisher(PublisherName),
-BookNo	int(10),
-foreign key (BookNo) references Book(BookNo),
-constraint Publisher_PK primary key(PublisherName, BookNo)
 
 );
 
@@ -126,11 +82,6 @@ constraint Publisher_PK primary key(PublisherName, BookNo)
 create table Author_Books
 (
 
-Fname	varchar(255),
-MName	varchar(255),
-LName	varchar(255),
-BookNo	int(10),
-foreign key (BookNo) references Book(BookNo)
 
 );
 
@@ -138,10 +89,7 @@ foreign key (BookNo) references Book(BookNo)
 create table Book_Holds
 (
 
-BookNo	int(10),
-foreign key (BookNo) references Book(BookNo),
 PatronNo	int(8),
-foreign key (PatronNo) references Patron(CardNo)
 
 );
 
@@ -149,11 +97,7 @@ foreign key (PatronNo) references Patron(CardNo)
 create table Book_History
 (
 
-BookNo	int(10),
-foreign key (BookNo) references Book(BookNo),
 PatronNo	int(8),
-foreign key (PatronNo) references Patron(CardNo),
-ReturnDate	date
 
 );
 
@@ -161,11 +105,7 @@ ReturnDate	date
 create table Book_Ratings
 (
 
-BookNo	int(10),
-foreign key (BookNo) references Book(BookNo),
 PatronNo	int(8),
-foreign key (PatronNo) references Patron(CardNo),
-rating	int(1)
 
 );
 
@@ -173,7 +113,6 @@ rating	int(1)
 create table Event_Staff
 (
 
-Fname	varchar(255)
 
 );
 
@@ -181,7 +120,6 @@ Fname	varchar(255)
 create table Event_Attendance
 (
 
-Fname	varchar(255)
 
 );
 
@@ -189,7 +127,6 @@ Fname	varchar(255)
 create table Payments
 (
 
-Fname	varchar(255)
 
 );
 
@@ -197,7 +134,6 @@ Fname	varchar(255)
 create table Genre
 (
 
-Fname	varchar(255)
 
 );
 
@@ -205,7 +141,6 @@ Fname	varchar(255)
 create table Phone_No
 (
 
-Fname	varchar(255)
 
 );
 
@@ -216,4 +151,3 @@ create table M_Name
 Fname	varchar(255)
 
 );
-
